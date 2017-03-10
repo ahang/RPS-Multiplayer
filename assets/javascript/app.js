ae2e7fd0fa1e346eb660390a1b151e0ca61fb353
dashaman <hang.andy.t@gmail.com>
2017-03-09 22:34:36 -0600

    v0.02

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
 
+    // Initialize Firebase
+    var config = {
+    apiKey: "AIzaSyAy3oio7aeJt92EEhN7_ogUYU1yEShljOU",
+    authDomain: "rps-multiplayer-355e1.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-355e1.firebaseio.com",
+    storageBucket: "rps-multiplayer-355e1.appspot.com",
+    messagingSenderId: "156084856160"
+    };
+
+    firebase.initializeApp(config);
+
+    //Grabbing a ref to the database service
+    var database = firebase.database();
+
+    database.ref().on("value", function(snapshot) {
+
+    });
+
+
 
 
 
