0e34096f50707b81e51b1fc79c67a176ad1016d1
FoxMessenger <liamfox@dhcp-10-105-92-244.wireless.northwestern.private>
2017-03-13 18:06:12 -0500

    added getPlayer set Player in javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,32 @@
 $(document).ready(function() {
 
     var maxPlayers = 2;
+    var playerOne = false;
+    var playerTwo = false;
+
+    function getPlayerOne() {
+    	return playerOne;
+    }
+
+    function setPLayerOne(val) {
+    	playerOne = val;
+    }
+
+    function getPlayerTwo() {
+    	return playerTwo;
+    }
+
+    function setPLayerTwo(val) {
+    	playerOne = val;
+    }
 
     // Initialize Firebase
     var config = {
-    apiKey: "AIzaSyAy3oio7aeJt92EEhN7_ogUYU1yEShljOU",
-    authDomain: "rps-multiplayer-355e1.firebaseapp.com",
-    databaseURL: "https://rps-multiplayer-355e1.firebaseio.com",
-    storageBucket: "rps-multiplayer-355e1.appspot.com",
-    messagingSenderId: "156084856160"
+	    apiKey: "AIzaSyAy3oio7aeJt92EEhN7_ogUYU1yEShljOU",
+	    authDomain: "rps-multiplayer-355e1.firebaseapp.com",
+	    databaseURL: "https://rps-multiplayer-355e1.firebaseio.com",
+	    storageBucket: "rps-multiplayer-355e1.appspot.com",
+	    messagingSenderId: "156084856160"
     };
 
     firebase.initializeApp(config);
@@ -18,17 +36,22 @@ $(document).ready(function() {
     $(".add-player-btn").on("click", function(event) {
         event.preventDefault();
 
+		
+		console.log('playerOne is: ' + getPlayerOne);
         var playerName = $(".name-input").val().trim();
-        if ()
-
-        var newPlayer = {
+        
+	
+        var playerOne = {
             name: playerName
         };
+	
 
-        database.ref().push(newPlayer);
+
+        database.ref().push(playerOne);
         console.log("Successfully add newPlayer");
 
         $("name-input").val("");
+      	// if (playerOne === false) {  }
     });
 
 
